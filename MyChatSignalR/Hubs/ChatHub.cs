ef16cffa6f8de0237be3928c7401b5d6d5566f83
Kakmusayan <mustafa.kakenov@gmail.com>
2021-03-10 00:51:31 +0600

    initial commit

---
+++ MyChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MyChatSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {       
+        // Отправка сообщений
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }       
+    }
+}
---
+++ MyChatSignalR/Scripts/util.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+
+    $('#chatBody').show();
+   // Ссылка на автоматически-сгенерированный прокси хаба
+    var chat = $.connection.chatHub;
+    // Объявление функции, которая хаб вызывает при получении сообщений
+    chat.client.addMessage = function (message) {
+        // Добавление сообщений на веб-страницу 
+        $('#chatroom').append('</b>: ' + htmlEncode(message) + '</p>');
+    };  
+
+    // Кодирование тегов
+    function htmlEncode(value) {
+        var encodedValue = $('<div />').text(value).html();
+        return encodedValue;
+    }
+
+ }
+   
+
+
